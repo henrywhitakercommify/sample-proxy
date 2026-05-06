62a3ed1c34eda49c0801909560852b5991bd1487
Henry Whitaker <henry.whitaker@commify.com>
2026-05-06 12:14:03 +0100

    fix: add fallback handler
    Signed-off-by: Henry Whitaker <henry.whitaker@commify.com>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use axum::{routing::any, Router};
 use tracing::info;
 use tracing_subscriber::EnvFilter;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     tracing_subscriber::fmt()
         .json()
         .with_env_filter(
@@ -10,5 +12,15 @@ fn main() {
         .with_line_number(true)
         .with_file(true)
         .init();
-    info!("here");
+
+    let app = Router::new().fallback(any(handler));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    info!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handler(req: axum::extract::Request) -> &'static str {
+    info!(method = %req.method(), uri = %req.uri(), headers = ?req.headers(), "request");
+    "ok"
 }
