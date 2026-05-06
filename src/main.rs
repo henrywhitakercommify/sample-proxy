31bbd9da070cdaf2131a711ef7f4351be9b126f9
Henry Whitaker <henry.whitaker@commify.com>
2026-05-06 12:08:26 +0100

    fix: add logging lib
    Signed-off-by: Henry Whitaker <henry.whitaker@commify.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use tracing::info;
+use tracing_subscriber::EnvFilter;
+
 fn main() {
-    println!("Hello, world!");
+    tracing_subscriber::fmt()
+        .json()
+        .with_env_filter(
+            EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info")),
+        )
+        .with_line_number(true)
+        .with_file(true)
+        .init();
+    info!("here");
 }
